de0c287c59293637c25647c5b0d204be62983e4a
Cristhian Ariel Bonilla Espinosa- Ceiba Software <cristhian.bonilla@ceiba.com.co>
2018-04-05 10:41:58 -0500

    eliminar cupo clases

--- src/main/java/com/ceiba/parking/ParkingApplication.java
+++ src/main/java/com/ceiba/parking/ParkingApplication.java
@@ -12,13 +12,7 @@ public class ParkingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ParkingApplication.class, args);
-		String example = "Ariel";
-	    String firstLetter  = "";
-
-	  
-	    firstLetter = String.valueOf(example.charAt(0));
-	    System.out.println("La primera letra "+firstLetter);
-
+		
 	 
 	
 	}
